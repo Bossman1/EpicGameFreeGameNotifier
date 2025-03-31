9526f7e59eb42d903e7f9797ec0d4d5fae6114ba
Nick Kharadze <vscodeart@gmail.com>
2025-03-31 22:42:51 +0400

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',['App\Http\Controllers\JobsGeController','index'])->name('index');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
